f6d1e3e86149d5fab8a7fbf8bb422d67546a4013
Bikash Pokharel <38802452+Dpakkk@users.noreply.github.com>
2022-04-28 15:23:51 -0400

    Update get_genre.py

--- src/get_genre.py
+++ src/get_genre.py
@@ -1,7 +1,15 @@
+'''
+This is the main file which we used to check the actual genre of mp3 file.
+'''
+
+
+# library here
+import pandas as pd
 import numpy as np
 import torch
 import sys
-
+import sklearn
+#import matplotlib.pyplot as plt
 from collections import Counter
 from sklearn.preprocessing import LabelEncoder
 
@@ -9,6 +17,7 @@ from librosa.core import load
 from librosa.feature import melspectrogram
 from librosa import power_to_db
 
+# calling our build-up model
 from model import genreNet
 from config import MODELPATH
 from config import GENRES
@@ -20,26 +29,22 @@ warnings.filterwarnings("ignore")
 def main(argv):
 
     if len(argv) != 1:
-        print("Usage: python3 get_genre.py audiopath")
+        print("run this file with the audio path")
         exit()
 
     le = LabelEncoder().fit(GENRES)
-    # ------------------------------- #
-    ## LOAD TRAINED GENRENET MODEL
+    ## load trained model
     net = genreNet()
     net.load_state_dict(torch.load(MODELPATH, map_location='cpu'))
-    # ------------------------------- #
-    ## LOAD AUDIO
+    ## load audio
     audio_path = argv[0]
     y, sr = load(audio_path, mono=True, sr=22050)
-    # ------------------------------- #
-    ## GET CHUNKS OF AUDIO SPECTROGRAMS
+    ## chunks of audio spec
     S = melspectrogram(y, sr).T
     S  = S[:-1 * (S.shape[0] % 128)]
     num_chunk = S.shape[0] / 128
     data_chunks = np.split(S, num_chunk)
-    # ------------------------------- #
-    ## CLASSIFY SPECTROGRAMS
+    ## classify spec
     genres = list()
     for i, data in enumerate(data_chunks):
         data = torch.FloatTensor(data).view(1, 1, 128, 128)
@@ -50,7 +55,6 @@ def main(argv):
         pred_genre = le.inverse_transform(pred_index).item()
         if pred_val >= 0.5:
             genres.append(pred_genre)
-    # ------------------------------- #
     s = float(sum([v for k,v in dict(Counter(genres)).items()]))
     pos_genre = sorted([(k, v/s*100 ) for k,v in dict(Counter(genres)).items()], key=lambda x:x[1], reverse=True)
     for genre, pos in pos_genre:
